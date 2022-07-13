20991f453c817535f1c6389a23a9fc9e2860e299
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-07-13 16:06:57 +0100

    Bugfix when checking secret value

--- .azure-pipelines/scripts/SetSqlServerAdminCredentials.ps1
+++ .azure-pipelines/scripts/SetSqlServerAdminCredentials.ps1
@@ -6,7 +6,7 @@ param (
 $keyVaultName = "kv-mx-pltfrm-$environment-$location"
 $secretName = "sql-platform-$environment-$location-admin-password"
 
-if ($null -eq (az keyvault secret show --name $secretName)) {
+if ($null -eq (az keyvault secret show --vault-name $keyVaultName --name $secretName)) {
     $password = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£$%^&*()".tochararray() | Sort-Object { Get-Random })[0..18] -join ''
     az keyvault secret set --name "$secretName" --vault-name $keyVaultName --value "$password" --description 'text/plain' | Out-Null
     az keyvault secret set --name "sql-platform-$environment-$location-admin-username" --vault-name $keyVaultName --value "sql-platform-$environment-$location-addy" --description 'text/plain' | Out-Null
