ab225f48dda8a1722fd785cbce83f11198e0b5c7
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-07-13 15:22:25 +0100

    Bugfixes

--- .azure-pipelines/scripts/SetSqlServerAdminCredentials.ps1
+++ .azure-pipelines/scripts/SetSqlServerAdminCredentials.ps1
@@ -8,6 +8,6 @@ $secretName = "sql-platform-$environment-$location-admin-password"
 
 if ($null -eq (az keyvault secret show --name $secretName)) {
     $password = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£$%^&*()".tochararray() | Sort-Object { Get-Random })[0..18] -join ''
-    az keyvault secret set --name $secretName --vault-name $keyVaultName --value $password --description 'text/plain' | Out-Null
+    az keyvault secret set --name "$secretName" --vault-name $keyVaultName --value "$password" --description 'text/plain' | Out-Null
     az keyvault secret set --name "sql-platform-$environment-$location-admin-username" --vault-name $keyVaultName --value "sql-platform-$environment-$location-addy" --description 'text/plain' | Out-Null
 }
